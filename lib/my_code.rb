048aa10cfcebf09af74c71da04cab96faafcacf1
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:21:37 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -42,7 +42,7 @@ end
 def reduce_to_total_starting_point(source_array, starting_point = 0)
   total = starting_point
   i = 0
-  while i < source_array.count
+  while i < source_array.count do
     total = total + source_array[i]
     i += 1
   end
