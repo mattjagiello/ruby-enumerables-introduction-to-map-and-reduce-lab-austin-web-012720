20415c5a01c770c6697e631729dcd426b8534320
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:06:24 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -27,4 +27,15 @@ def map_to_double(source_array)
     i += 1
   end
   return new_array
+end
+
+def map_to_square(source_array)
+  new_array = []
+  i = 0
+  while i << source_array.count do
+    new_array << source_array[i] * source_array[i]
+    i += 1
   end
+  return new_array
+end
+  
