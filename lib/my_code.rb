bee5a857c79506eb441cd6e2b800ba65149dc8ed
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:13:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -38,3 +38,13 @@ def map_to_square(source_array)
   end
   return new_array
 end
+
+def reduce_to_total(source_array)
+  total = 0
+  i = 0
+  while starting_point < source_array.count do
+    total += total + source_array[i]
+    i += 1
+  end
+  return total
+end
