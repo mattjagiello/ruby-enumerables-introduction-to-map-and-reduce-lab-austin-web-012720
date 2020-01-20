f5ce9b2357b24392adc409965e3bb5a0986b6410
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:07:57 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -33,7 +33,7 @@ def map_to_square(source_array)
   new_array = []
   i = 0
   while i << source_array.count do
-    new_array << source_array[i] * source_array[i]
+    new_array << (source_array[i] * source_array[i])
     i += 1
   end
   return new_array
