350848e2e6654d03d3444647e243ccfffdcf8636
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:25:50 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -48,3 +48,13 @@ def reduce_to_total(source_array, starting_point = 0)
   end
   return total
 end
+
+def reduce_to_all_true(source_array)
+  i = 0
+  while i < source_array.count do
+    if source_array[i] == false
+      return false
+    end
+    index =+ 1
+  return true
+  end
