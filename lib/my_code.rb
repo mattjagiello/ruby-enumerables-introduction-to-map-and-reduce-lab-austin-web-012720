8be07f83a288ffc3129953edc9d4edaeb92f40d6
Matthew Jagiello <mjagiello@gmail.com>
2020-01-20 18:03:39 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -8,3 +8,13 @@ def map_to_negativize(source_array)
   end
   return new_array
 end
+
+def map_to_no_change(source_array)
+  new_array = []
+  i = 0
+  while i < source_array.count do
+    new_array << source_array[i]
+    i += 1
+  end
+  return new_array
+end
